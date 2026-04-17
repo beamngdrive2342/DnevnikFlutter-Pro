3e4e9671fb23c27cb9d4a4314e32db9aa2b36579
beamngdrive2342 <beamngdrive2342@gmail.com>
2026-04-17 23:40:13 +0300

    Delete fix_main.ps1

--- fix_main.ps1
+++
@@ -1,6 +0,0 @@
-$header = Get-Content 'c:\src\DnevnikFlutter\lib\main_header.dart.tmp'
-$main = Get-Content 'c:\src\DnevnikFlutter\lib\main.dart'
-$tail = $main[373..($main.Count-1)]
-$result = $header + $tail
-$result | Set-Content 'c:\src\DnevnikFlutter\lib\main.dart' -Encoding UTF8
-Write-Host "Done. Total lines: $($result.Count)"
