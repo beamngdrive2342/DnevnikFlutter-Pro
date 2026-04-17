ba111c88174bd6663ff1e2432e487c307537d636
beamngdrive2342 <beamngdrive2342@gmail.com>
2026-04-17 23:40:31 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dnevnik_app/main.dart';
+import 'package:dnevnik_app/screens/auth_gate.dart';
 
 void main() {
   testWidgets('App shows role selector on launch', (WidgetTester tester) async {
